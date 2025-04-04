b7d82aaba9a5072a257b30b503890b3a331846dc
Clayton Galea <claytongalea78@outlook.com>
2025-04-04 09:46:10 +0200

    Initial commit (with .gitignore in place)

---
+++ source/repos/Clayton_EP_Assignment/Clayton_EP_Assignment/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Clayton_EP_Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clayton_EP_Assignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ source/repos/Clayton_EP_Assignment/Clayton_EP_Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Clayton_EP_Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
